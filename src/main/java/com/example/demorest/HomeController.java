4915f39a05ab5918582059820d3f05fa69e85464
Sourabh Bhukan <sourabhbhukan09@gmail.com>
2025-11-10 16:42:22 +0530

    Update HomeController.java
    crossorigin added

--- src/main/java/com/example/demorest/HomeController.java
+++ src/main/java/com/example/demorest/HomeController.java
@@ -14,7 +14,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/home")
 public class HomeController {
